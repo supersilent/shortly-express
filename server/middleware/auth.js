9d1c1908fc5197dae67c60fd8ea651e2cdf4dfdf
supersilent <shrikethrush@gmail.com>
2018-12-07 21:48:11 -0800

    Passed a test.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,19 +5,20 @@ const Session = require("../models/session");
 const User = require("../models/user");
 
 module.exports.createSession = (req, res, next) => {
-  cookie(req, res);
+  console.log(req.cookies);
   if (!Object.keys(req.cookies).length) {
+    cookie(req, res);
     Session.create().then(result => {
-      console.log(result);
+      // console.log(result);
       Session.get({ id: result.insertId }).then(results => {
         req.session = {};
         req.session.hash = results.hash;
         res.cookies = {};
         res.cookies.shortlyid = {};
         res.cookies.shortlyid.value = results.hash;
-        console.log(results);
+        // console.log(results);
         if (results.userId) {
-          console.log("doko");
+          // console.log("doko");
           User.getUsername(results.userId).then(queryUser => {
             req.session.user = {};
             req.session.user.username = queryUser.username;
@@ -30,6 +31,25 @@ module.exports.createSession = (req, res, next) => {
       });
     });
   } else {
+    req.session = {};
+    req.session.hash = req.cookies.shortlyid;
+    Session.get({ hash: req.cookies.shortlyid }).then(results => {
+      res.cookies = {};
+      res.cookies.shortlyid = {};
+      res.cookies.shortlyid.value = req.cookies.shortlyid;
+      // console.log(results);
+      if (results.userId) {
+        // console.log("doko");
+        User.getUsername(results.userId).then(queryUser => {
+          req.session.user = {};
+          req.session.user.username = queryUser.username;
+          req.session.userId = results.userId;
+          next();
+        });
+      } else {
+        next();
+      }
+    });
   }
 };
 
